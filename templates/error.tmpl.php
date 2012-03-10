f37aee6d35224efaeb1cd350d09cc39cc248d2fa
Jake Billo <jake@jakebillo.com>
2012-03-10 11:38:16 -0500

    Add error template

---
+++ templates/error.tmpl.php
@@ -0,0 +1,5 @@
+<div class="grid_12 box status status-fail">
+    <img src="<?php echo $ROOT; echo $theme['icons']['status-fail']; ?>" />
+    <?php echo $error_message; ?><br /><br />
+    <p><a href="javascript:history.go(-1)">Back to Previous</a></p>
+</div>
