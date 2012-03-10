afd7a2eda4772c569fb4fc0f462e505a5f360724
Jake Billo <jake@jakebillo.com>
2012-03-10 13:28:36 -0500

    Added example DB connection file

---
+++ db.example.php
@@ -0,0 +1,8 @@
+<?php
+$db_settings = array(
+    'host' => '/var/run/postgresql',
+    'user' => 'chintzy',
+    'password' => 'ch1ntzyCMS!',
+    'type' => 'postgres8',
+    'db' => 'chintzydb',
+);
