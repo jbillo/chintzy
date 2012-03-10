7c0e0a2f1b03c2a9896df210d5570618e4c62808
Jake Billo <jake@jakebillo.com>
2012-03-10 10:57:15 -0500

    Pushed actual file contents

--- reset_install.sh
+++ reset_install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-# Check for db.inc.php and reset installation if required
+USERNAME=jbillo
+WEBGROUP=www-data
 
-# rm db.inc.php
-sudo chown -R jbillo:www-data *
-chgrp -R www-data cache log
+sudo chown -R ${USERNAME}:${WEBGROUP} *
+chgrp -R ${WEBGROUP} -data cache log
 chmod -R g+w cache log
 rm -rf cache/*
 rm -rf log/*
-chgrp -R www-data libs/htmlpurifier/standalone/HTMLPurifier/DefinitionCache/Serializer
+chgrp -R ${WEBGROUP} libs/htmlpurifier/standalone/HTMLPurifier/DefinitionCache/Serializer
 chmod -R g+w libs/htmlpurifier/standalone/HTMLPurifier/DefinitionCache/Serializer
 
 echo Chintzy has been reset!
