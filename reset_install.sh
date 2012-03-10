3db0107905749009fb7dd444bb9a92439a6d1167
Jake Billo <jake@jakebillo.com>
2012-03-10 11:37:19 -0500

    Fix reset_install

--- reset_install.sh
+++ reset_install.sh
@@ -4,7 +4,7 @@ USERNAME=jbillo
 WEBGROUP=www-data
 
 sudo chown -R ${USERNAME}:${WEBGROUP} *
-chgrp -R ${WEBGROUP} -data cache log
+chgrp -R ${WEBGROUP} cache log
 chmod -R g+w cache log
 rm -rf cache/*
 rm -rf log/*
